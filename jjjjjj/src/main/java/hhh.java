0ae2dadae81083ef3972ebd90b42e7d04f08ab20
tianhuage1997 <FENGtong521314>
2019-04-28 10:18:42 +0800

    Initial commit

---
+++ jjjjjj/src/main/java/hhh.java
@@ -0,0 +1,20 @@
+public class hhh {
+    /**
+     *
+     * 如果是引用了spring或者springboot的话，
+     * @Autowired  自动注入的注解
+     * mybatis
+     * sql>mapper.xml文件
+     * 接口是在spring里面的接口类
+     * 由于idea里面没有检测到spring里面的接口类的实现类。
+     * 就会出现注入出错的问题
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+        System.out.println(1);
+    }
+}
